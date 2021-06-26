e57d8ea624d1569525659e0639328570123033f2
hogang1223 <hyogangster1@gmail.com>
2021-06-26 22:46:51 +0900

    connect cart to image detail view

--- app/src/main/java/com/aosproject/imagemarket/Util/ShareVar.java
+++ app/src/main/java/com/aosproject/imagemarket/Util/ShareVar.java
@@ -2,7 +2,7 @@ package com.aosproject.imagemarket.Util;
 
 public class ShareVar {
     //public static final String macIP = "http:// 본인 아이피 주소  :8080/imagemarket/";
-    public static final String macIP = "http://172.30.115.138:8080/imagemarket/";
+    public static final String macIP = "http://192.168.219.104:8080/imagemarket/";
     public static final String hjIP = "";
     public static final String hgIP = "";
     public static final String dyIP = "";
