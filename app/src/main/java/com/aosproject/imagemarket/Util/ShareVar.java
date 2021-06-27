523e22b726bdc425433ccd4605af998e8eea1c99
hogang1223 <hyogangster1@gmail.com>
2021-06-27 17:28:31 +0900

    change download mail address

--- app/src/main/java/com/aosproject/imagemarket/Util/ShareVar.java
+++ app/src/main/java/com/aosproject/imagemarket/Util/ShareVar.java
@@ -3,6 +3,7 @@ package com.aosproject.imagemarket.Util;
 public class ShareVar {
 
     public static final String macIP = "http://172.30.1.22:8080/imagemarket/";
-
     public static  String loginEmail = "";
+    public static String EMAIL = "jo.globaltour@gmail.com";
+    public static String PASSWORD = "global@2020";
 }
