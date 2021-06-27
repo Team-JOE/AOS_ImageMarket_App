02f682aeca9633fbac75a3bb2d421517a01a35e1
iDoyoung <ido.plum@gmail.com>
2021-06-27 12:56:12 +0900

    Fix popular

--- app/src/main/java/com/aosproject/imagemarket/Fragment/RecommendFragment.java
+++ app/src/main/java/com/aosproject/imagemarket/Fragment/RecommendFragment.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.GridView;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -20,6 +21,9 @@ import com.aosproject.imagemarket.Bean.JsonImageDY;
 import com.aosproject.imagemarket.NetworkTask.ImageNetworkTaskDY;
 import com.aosproject.imagemarket.R;
 import com.aosproject.imagemarket.Util.ShareVar;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.FitCenter;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
 
 import java.util.ArrayList;
 
@@ -30,6 +34,7 @@ public class RecommendFragment extends Fragment {
     PopularImageAdapter adapter;
     ArrayList<JsonImageDY> images = new ArrayList<JsonImageDY>();
     String urlAddr = null;
+    ImageView top1,top2,top3,top4;
 
     @Nullable
     @Override
@@ -39,7 +44,11 @@ public class RecommendFragment extends Fragment {
         photo = view.findViewById(R.id.layout_show_photo);
         illust = view.findViewById(R.id.layout_show_illust);
         calligraphy = view.findViewById(R.id.layout_show_calligraphy);
-        gridView = view.findViewById(R.id.grid_popular);
+        //gridView = view.findViewById(R.id.grid_popular);
+        top1 = view.findViewById(R.id.image_top_1);
+        top2 = view.findViewById(R.id.image_top_2);
+        top3 = view.findViewById(R.id.image_top_3);
+        top4 = view.findViewById(R.id.image_top_4);
 
         urlAddr = ShareVar.macIP +"/jsp/popular_select.jsp";
         Log.i("JSP",urlAddr);
@@ -95,8 +104,12 @@ public class RecommendFragment extends Fragment {
             ImageNetworkTaskDY networkTask = new ImageNetworkTaskDY(getContext(), urlAddr, "select");
             Object obj = networkTask.execute().get();
             images = (ArrayList<JsonImageDY>) obj;
-            adapter = new PopularImageAdapter(getContext(), R.layout.card_grid_popular, images);
-            gridView.setAdapter(adapter);
+            Glide.with(this).load(ShareVar.macIP + "image/" + images.get(0).getImageFilepath()).into(top1);
+            Glide.with(this).load(ShareVar.macIP + "image/" + images.get(1).getImageFilepath()).into(top2);
+            Glide.with(this).load(ShareVar.macIP + "image/" + images.get(2).getImageFilepath()).into(top3);
+            Glide.with(this).load(ShareVar.macIP + "image/" + images.get(3).getImageFilepath()).into(top4);
+//            adapter = new PopularImageAdapter(getContext(), R.layout.card_grid_popular, images);
+//            gridView.setAdapter(adapter);
         }catch (Exception e){
             e.printStackTrace();
         }
