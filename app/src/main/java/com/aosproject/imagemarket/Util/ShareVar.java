ea49d69278cd77b1e0e88619d79f5ac106df8339
hogang1223 <hyogangster1@gmail.com>
2021-06-27 11:59:54 +0900

    add mail library

--- app/src/main/java/com/aosproject/imagemarket/Util/ShareVar.java
+++ app/src/main/java/com/aosproject/imagemarket/Util/ShareVar.java
@@ -2,7 +2,7 @@ package com.aosproject.imagemarket.Util;
 
 public class ShareVar {
 
-    public static final String macIP = "http://172.30.1.38:8080/imagemarket/";
+    public static final String macIP = "http://172.30.1.22:8080/imagemarket/";
 
     public static  String loginEmail = "";
 }
